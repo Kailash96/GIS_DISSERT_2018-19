412c73188aa66df85d0ad662408ca92c80899c4b
Kailash96 <kailash_chooramun@hotmail.com>
2018-12-12 14:48:06 +0400

    main page in progress

---
+++ project/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home | GoGreen</title>
+        <link rel="stylesheet" href="css_files/style.css" />
+    </head>
+    <body>
+        <?php include("left_nav_bar.php"); ?>
+    </body>
+</html>
---
+++ project/left_nav_bar.php
@@ -0,0 +1,26 @@
+<div class="sidebar_left">
+    <table>
+        <tr>
+            <td><img src="" style="width:60px;height:60px;"></td>
+            <td>District Council of Flacq(example - to be modified)</td>
+        </tr>
+    </table>
+    <br/>
+    <b>Services</b>
+    <ul class="left_links">
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Notifications</a>
+        </li>
+        <li>
+            Zones
+            <ul>
+                <li>Zone 1</li>
+                <li>Zone 2</li>
+                <li>Zone 3</li>
+            </ul>
+        </li>
+    </ul>
+</div>
