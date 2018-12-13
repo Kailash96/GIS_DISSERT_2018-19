4b8d4b9e30a70c2cc1068cbc5782f065a58e72e2
Kailash96 <kailash_chooramun@hotmail.com>
2018-12-13 11:44:16 +0400

    keeping copy - map not added to main page

---
+++ project/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home | goGreen</title>
+        <link rel="stylesheet" href="css_files/style.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+            <?php include("left_side_nav_bar.html"); ?>
+        <div class="main_container">
+            
+        </div>
+    </body>
+</html>
