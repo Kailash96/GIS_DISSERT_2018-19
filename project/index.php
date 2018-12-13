8930e7bd667cf7a9d37f2bb86d95b63ad1a76829
Kailash96 <kailash_chooramun@hotmail.com>
2018-12-13 14:04:58 +0400

    logo added with new changes - backing up

--- project/index.php
+++ project/index.php
@@ -4,11 +4,19 @@
         <title>Home | goGreen</title>
         <link rel="stylesheet" href="css_files/style.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <style>
+            html, body, #viewDiv {
+            padding: 0;
+            margin: 0;
+            height: 100%;
+            width: 100%;
+            }
+        </style>
     </head>
     <body>
-            <?php include("left_side_nav_bar.html"); ?>
-        <div class="main_container">
-            
-        </div>
+        <?php include("left_side_nav_bar.html"); ?>
+        <?php include("top-nav-bar.html"); ?>
+        
     </body>
 </html>
