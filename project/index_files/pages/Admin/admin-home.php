e635899814ba04e23755442f8bff47d3cb1eb3bf
Kailash96 <kailash_chooramun@hotmail.com>
2019-01-06 08:17:17 +0400

    new changes - 6jan2019

---
+++ project/index_files/pages/Admin/admin-home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin | Binswiper</title>
+        <link rel="stylesheet" href="../../css_files/style.css" />
+
+    </head>
+    <body>
+        <?php include("admin-left_side_nav_bar.html"); ?>
+        <?php include("admin-top-nav-bar.html"); ?>
+        
+    </body>
+</html>
--- project/index_files/pages/Resident-pages/resident-registration.php
+++ project/index_files/pages/Resident-pages/resident-registration.php
@@ -4,10 +4,26 @@
         <title>Resident Registration | Binswiper</title>
         <link type="text/css" rel="stylesheet" href="../../css_files/style.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
+        integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+        crossorigin=""/>
+        <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
+        integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
+        crossorigin=""></script>
+        <script>
+            var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox.streets',
+                accessToken: 'your.mapbox.access.token'
+            }).addTo(mymap);
+        </script>
         <style>
             *{
                 color:#002246;
             }
+
             .reg-form-box{
                 display:grid;
                 grid-template-columns: auto auto;
@@ -31,6 +47,11 @@
             .submit_button:hover{
                 box-shadow:0 0 10px #002246;
             }
+
+            #mapid{
+                height:180px;
+            }
+            
         </style>
     </head>
     <body style="margin:0;padding:0;">
@@ -51,7 +72,7 @@
                     </form>
                 </div>
                 <div>
-                    map
+                    <div id="mapid"></div>
                 </div>
             </div>
         </div>
