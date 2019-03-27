a0959204d2923c996eaa3d15e27f34bcc83c959b
Kailash96 <kailash_chooramun@hotmail.com>
2019-03-28 02:22:48 +0400

    changes made

--- project/index_files/pages/scheduling/scheduling_script.js
+++ project/index_files/pages/scheduling/scheduling_script.js
@@ -75,7 +75,7 @@ function configure_trip(trip_array){
         for (var TA = 1; TA < trip_array[t][0].length; TA++) {
             var pointA = trip_array[t][0][TA - 1].split(",");
             var pointB = trip_array[t][0][TA].split(",");
-            var collection_delay = 3; // 3MINS COLLECTION TIME BUFFER
+            var collection_delay = 60; // 3; // 3MINS COLLECTION TIME BUFFER
             total_duration = (getDuration(pointA, pointB) + collection_delay); // IN MINS
             total_distance += getDistance(pointA, pointB) / 1000; // IN KM
         }
