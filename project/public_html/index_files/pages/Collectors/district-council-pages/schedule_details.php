b976a8d7e28d6daba3466dc5bc82b81981d4d22c
unknown <kailash_chooramun@hotmail.com>
2019-05-05 23:25:39 +0400

    working calendar

--- project/public_html/index_files/pages/Collectors/district-council-pages/schedule_details.php
+++ project/public_html/index_files/pages/Collectors/district-council-pages/schedule_details.php
@@ -1,22 +1,40 @@
 <?php
     include("../../../../../config/db_connect.php");
 
-    $data = array();
+    class calendar {
+        public function __construct($title, $start, $end) {
+            $this->title = $title;
+            $this->start = $start;
+            $this->end = $end;
+            $this->color = "yellow";
+        }
+    }
 
-    //$counter = 0;
+    $data = array(); // ""; // "[";
+
+    // $counter = 0;
     // count number of rows
-    $count_query = "SELECT * FROM tbl_schedule";
-    $count_rw = mysqli_num_rows(mysqli_query($conn, $count_query));
+    //$count_query = "SELECT * FROM tbl_schedule";
+    //$count_rw = mysqli_num_rows(mysqli_query($conn, $count_query));
 
     $schedule_query = "SELECT * FROM tbl_schedule";
     if ($count = mysqli_num_rows($schedules = mysqli_query($conn, $schedule_query)) > 0) {
         while ($sch = mysqli_fetch_assoc($schedules)) {
-            //$counter++;
+            $title = $sch['TruckID'];
+            $start = $sch['CollectionDate'] . "T" . $sch['TimeStart'];
+            $end = $sch['CollectionDate'] . "T" . $sch['TimeEnd'];
+            // $counter++;
             
-            $data_value = "title: '" . $sch['TruckID'] . "', start: '" . $sch['CollectionDate'] . "T" . $sch['TimeStart'] . "', end: '" . $sch['CollectionDate'] . "T" . $sch['TimeEnd'] . "', color: 'Yellow'";
+            // CREATE OBJECT AND PUSH TO ARRAY
+            $data_object = new calendar($title, $start, $end);
+            array_push($data, $data_object);
 
-            array_push($data, $data_value);
+             // $data_value = 'title: "' . $sch["TruckID"] . '", start: "' . $sch["CollectionDate"] . 'T' . $sch["TimeStart"] . '", end: "' . $sch["CollectionDate"] . 'T' . $sch["TimeEnd"] . '", color: "Yellow"';
+
+            // array_push($data, $data_value);
             
+           //  $data .= 'title: "' . $sch["TruckID"] . '", start: "' . $sch["CollectionDate"] . 'T' . $sch["TimeStart"] . '", end: "' . $sch["CollectionDate"] . 'T' . $sch["TimeEnd"] . '", color: "Yellow"';
+
             /*
             array_merge($data, array(
                 'title' => '" . $sch['TruckID'] . "',
@@ -28,12 +46,13 @@
             
             
             if ($counter < $count_rw) {
-                $data .= ",";
+                $data .= "]";
             }
             */
         }
     }
 
+    //$data .= "]";
 
-    echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    echo json_encode($data);
 ?>
--- project/public_html/index_files/pages/Collectors/district-council-pages/schedules.php
+++ project/public_html/index_files/pages/Collectors/district-council-pages/schedules.php
@@ -11,155 +11,18 @@
             }
         </style>
         <script>
-
+            // TEST DATA
             /*
-            var data = JSON.stringify([    
+            var data = [
                 {
-                title: 'DC1020 zone:20',
-                start: '2019-03-04T05:00:00',
-                end: '2019-03-04T07:30:00',
-                color: 'yellow'
+                    title: '10Z11',
+                    start: '2019-04-08T05:15:00',
+                    end: '2019-04-08T05:54:00',
+                    color: 'yellow'
                 }
-                ,
-                {
-                title: 'DC1020 zone:23',
-                start: '2019-03-04T08:00:00',
-                end: '2019-03-04T09:30:00',
-                color: 'yellow'
-                }
-            ]);
-            console.log(data);
+            ];
             */
-                /*,
-                {
-                title: 'DC1020 zone:23',
-                start: '2019-03-04T10:00:00',
-                end: '2019-03-04T12:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'DC1020 zone:23',
-                start: '2019-03-05T05:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'A2010 zone:24',
-                start: '2019-03-05T09:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'A2010 zone:25',
-                start: '2019-03-06T05:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'A2010 zone:26',
-                start: '2019-03-06T09:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'B3030 zone:27',
-                start: '2019-03-06T11:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'B3030 zone:28',
-                start: '2019-03-06T13:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'T10Z11 zone:28',
-                start: '2019-03-07T06:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'B3030 zone:30',
-                start: '2019-03-07T13:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'T10Z11 zone:29',
-                start: '2019-03-07T13:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'A2010 zone:30',
-                start: '2019-03-08T08:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'A2010 zone:31',
-                start: '2019-03-08T13:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'B1010 zone:32',
-                start: '2019-03-09T07:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'B1010 zone:33',
-                start: '2019-03-09T09:00:00',
-                color: 'yellow'
-                },
-                {
-                title: 'A2010 zone:34',
-                start: '2019-03-11T07:00:00',
-                color: 'yellow'
-                }
-                */
-                /*
-                {
-                title: 'Meeting',
-                start: '2019-03-13T11:00:00',
-                end: '2019-03-13T13:00:00',
-                color: '#257e4a'
-                },
-                {
-                title: 'Business Lunch',
-                start: '2019-03-13T13:00:00',
-                },
-                {
-                title: 'Business Lunch',
-                start: '2019-03-13T13:00:00',
-                },
-
-                
-                {
-                title: 'Conference',
-                start: '2019-03-18',
-                end: '2019-03-20'
-                },
-                {
-                title: 'Party',
-                start: '2019-03-29T20:00:00'
-                },
-
-                // areas where "Meeting" must be dropped
-                {
-                groupId: 'availableForMeeting',
-                start: '2019-03-11T10:00:00',
-                end: '2019-03-11T16:00:00',
-                rendering: 'background'
-                },
-                {
-                groupId: 'availableForMeeting',
-                start: '2019-03-13T10:00:00',
-                end: '2019-03-13T16:00:00',
-                rendering: 'background'
-                },
-
-                // red areas where no events can be dropped
-                {
-                start: '2019-03-24',
-                end: '2019-03-28',
-                overlap: false,
-                rendering: 'background',
-                color: '#ff9f89'
-                }
-                
-            ]);
-            */
+            
         </script>
     </head>
     <body>
